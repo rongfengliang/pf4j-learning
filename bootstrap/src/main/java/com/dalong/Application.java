243faa37fe6ab978fc14787ff6c420d3ff9519e6
rong fengliang <1141591465@qq.com>
2022-02-17 13:22:19 +0800

    add more

--- bootstrap/src/main/java/com/dalong/Application.java
+++ bootstrap/src/main/java/com/dalong/Application.java
@@ -3,6 +3,7 @@ package com.dalong;
 import org.pf4j.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class Application {
@@ -13,6 +14,16 @@ public class Application {
                 DefaultExtensionFinder extensionFinder= (DefaultExtensionFinder) super.createExtensionFinder();
                 extensionFinder.addServiceProviderExtensionFinder();
                 return extensionFinder;
+            }
+
+            @Override
+            public ClassLoader getPluginClassLoader(String pluginId) {
+                return super.getPluginClassLoader(pluginId);
+            }
+
+            @Override
+            protected Map<String, ClassLoader> getPluginClassLoaders() {
+                return super.getPluginClassLoaders();
             }
 
             @Override
